8aa0df734d97a08cd8cfcc4b964535edb926997f
Tyler <mosstp@mail.uc.edu>
2026-01-14 17:15:29 -0500

    Firebase push

--- src/app/firebase/firebase.config.ts
+++ src/app/firebase/firebase.config.ts
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "-",
-  authDomain: "-",
-  projectId: "-",
-  storageBucket: "-",
-  messagingSenderId: "-",
-  appId: "-",
-  measurementId: "-"
+  apiKey: "AIzaSyCOjx0Q_2U3c-sCjThN9WUIkR-iIVL5Zhk",
+  authDomain: "leasevault-db.firebaseapp.com",
+  projectId: "leasevault-db",
+  storageBucket: "leasevault-db.firebasestorage.app",
+  messagingSenderId: "772088849600",
+  appId: "1:772088849600:web:fbd6c884bc3d05fb10b925",
+  measurementId: "G-J64LBE4V02"
 };
 
 // Initialize Firebase
